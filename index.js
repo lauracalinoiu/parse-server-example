3d8fd0b3af349f45704e6471ca193f6c6209c821
Laura C <lauramocanu16@gmail.com>
2016-02-22 11:20:33 +0200

    check if appId needs indeed to be defined in env

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'reciparia',
+  appId: 'reciparia',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   clientKey: 'holla'
 });
